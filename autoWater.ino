bbcece32d29224475e7764c7c23b1b7fe5e76811
jonsolv <33338345+jonsolv@users.noreply.github.com>
2018-05-12 16:53:40 +1200

    Update autoWater.ino

--- autoWater.ino
+++ autoWater.ino
@@ -1,9 +1,11 @@
 // TM1638lite - Version: Latest
 #include <TM1638lite.h>
-TM1638lite tm(7, 9, 8); //(strobe, clock, data)
+#define SOIL A0; //connected to moisture reader
+#define STROBE 7;
+#define CLOCK 9;
+#define DATA 8;
+TM1638lite tm(STROBE, CLOCK, DATA); //(strobe, clock, data)
 
-#define SOIL A0 //connected to moisture reader
-int moist; //read moisture value
 int readings = 1000; //number of reading to take for average
 uint8_t memButtons = 0; //buttons pressed
 float plotTime = 600000; //milliseconds between plots
@@ -13,11 +15,7 @@ void setup() {
   pinMode(SOIL, INPUT);
   Serial.begin(57600);
   tm.displayText("Starting");
-  delay(500);
-
 }
-
-
 
 int getAverageMoisture() {
   float totalread = 0;
@@ -34,32 +32,29 @@ int getRealMoisture() {
 void loop() {
   uint8_t buttons = tm.readButtons();
   if (buttons > 0) { memButtons = buttons; }
+  if (memButtons == 1) {
+    tm.displayText(String(getAverageMoisture()/10)); //display average 1-10
+  }
   if (memButtons == 2) {
-    tm.displayText(String(getRealMoisture())); //full reading
+    tm.displayText(String(getRealMoisture())); //full reading live data
   }
   if (memButtons == 4) {
-    tm.displayText(String(getAverageMoisture())); //full reading /100
+    tm.displayText(String(getAverageMoisture())); //average data full reading
   }
-    if (memButtons == 8) {
-    tm.displayText(String(getRealMoisture()/10)); //full reading /100
+  if (memButtons == 8) {
+    //4th button
   }
-  if (memButtons == 128) {
-    readings += 10;
-    String Disp = "A";
-    tm.displayText(Disp + String(readings));
-    memButtons = 3;
-    delay(100);
+  if (memButtons == 16) {
+    //5th button
   }
+  if (memButtons == 32) {
+    //6th button
+  }  
   if (memButtons == 64) {
-    readings -= 10;
-    String Disp = "A";
-    tm.displayText(Disp + String(readings));
-    memButtons = 3;
-    delay(100);
-  }
-
-  if (memButtons == 1) {
-    tm.displayText(String(getAverageMoisture()/10)); //display 1-10
+  //7th
+  }  
+  if (memButtons == 128) {
+  //8th
   }
 if (millis() > (lastPlot + plotTime)) {
   lastPlot = millis();
